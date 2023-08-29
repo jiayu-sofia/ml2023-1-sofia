3e9b991fbbb66fa7bb9dcf86074c35cfec4b4b35
jiayu-sofia <142766404+jiayu-sofia@users.noreply.github.com>
2023-08-28 23:15:54 -0700

    Create module4.py

---
+++ module4.py
@@ -0,0 +1,21 @@
+value = input('Enter your value: ')
+print('Value entered: ', value)
+
+N = input('Enter a positive number: ')
+print('Number entered: ', N)
+
+dict = {}
+for i in range(int(N)):
+    value = input('Enter your value: ')
+    print('Value entered: ', value)
+    dict[i] = value
+
+inal_value = input('Enter your value: ')
+k = 0
+for i in list(dict.keys()):
+    if dict[i] == final_value:
+        print('Index of your input: ',i)
+        k = 1
+        break
+if k==0:
+    print('Index of your input: ',-1)
