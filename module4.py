880d65d64a3eb42ff6f93448b89ae4efa90eddb7
jiayu-sofia <142766404+jiayu-sofia@users.noreply.github.com>
2023-08-28 23:20:20 -0700

    Update module4.py

--- module4.py
+++ module4.py
@@ -14,7 +14,7 @@ inal_value = input('Enter your value: ')
 k = 0
 for i in list(dict.keys()):
     if dict[i] == final_value:
-        print('Index of your input: ',i)
+        print('Index of your input: ',i+1)
         k = 1
         break
 if k==0:
